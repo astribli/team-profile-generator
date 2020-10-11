66d66014f13ffc6a76758167907970dbab6c4b02
astribli <andrewjstribling@gmail.com>
2020-10-11 00:53:59 -0500

    build classes

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
+// Requires
 const fs = require('fs');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
+const inquirer = require('inquirer');
+const path = require('path');
 const generatePage = require('./src/page-template.js');
+
+// Array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give project description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please give installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please give usage information:'
+    },
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: 'Please give contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please give test instructions:'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please choose a license for your project:',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'none']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:'
+    },
+];
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
